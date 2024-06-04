a1c691ea6859e4d77315dc090b555d51103beaad
DARKkek <semen.a.ches62@gmail.com>
2024-06-04 14:48:48 +0700

    сделал отображение продуктов из бд на сайте и еще куча фиксов

--- OnlineFoodShop/OnlineFoodShop/urls.py
+++ OnlineFoodShop/OnlineFoodShop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('catalog.urls', namespace='catalog'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- OnlineFoodShop/catalog/models.py
+++ OnlineFoodShop/catalog/models.py
@@ -18,7 +18,7 @@ class Products(models.Model):
     image = models.ImageField(upload_to='catalog_images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка')
-    # мб количество продуктов сюда
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
     class Meta:
